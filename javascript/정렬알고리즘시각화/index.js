ddf27b7681b6516ebcdaef753b8cc468d546b2b6
Joylish <yeonzzu2@gmail.com>
2020-08-27 21:01:09 +0900

    선택정렬 어느정도

--- javascript/정렬알고리즘시각화/index.js
+++ javascript/정렬알고리즘시각화/index.js
@@ -1,25 +1,7 @@
-// const showButton = document.getElementById("showButton")
-
-// showButton.addEventListener('click', function() {
-//   const circles = document.getElementById("circles");
-//   const numbers = document.getElementById('numbers').value.split(",");
-//   if (numbers.length < 2){
-//     alert('1개 이상 숫자를 입력해주세요.');
-//     return;
-//   }
-//   numbers.map((number) => {
-//     const numberElement = document.createElement("div");
-//     numberElement.className = "circle";
-//     numberElement.innerText = number;
-//     circles.appendChild(numberElement);  
-//     return;
-//   });
-//   this.removeEventListener('click', arguments.callee); //arguments.callee 를 사용하여 스스로의 함수를 지정하여 제거
-// });
-
-const showSorts = () => {
+const selectSortType = () => {
   document.getElementById("dropdown-contents").classList.toggle("show");
 };
+
 
 const getNumbers = () => {
   const numbers = document.getElementById("numbers").value.split(",");
@@ -27,17 +9,78 @@ const getNumbers = () => {
 };
 
 const checkNumbers = (numbers)=>{
-    if (numbers.length < 2){
+  if (numbers.length < 2){
       alert('1개 이상 숫자를 입력해주세요.');
       return -1; //비정상
     }
     return 0 //정상
 }
 
-const selectSort = () =>{
+const createCircles = (numbers) => {
+  const circles = document.getElementById("numbers");
+  while (circles.hasChildNodes()) {
+    circles.removeChild(circles.firstChild);
+  }
+  const numberList = document.createElement("ul");
+  numberList.id = "numbers-list";
+  numberList.className = "numbers-list";
+
+  numbers.map((number) => {
+    const numberElement = document.createElement("li");
+    numberElement.className = "number";
+    numberElement.innerText = number;
+    numberList.appendChild(numberElement);
+    return;
+  });
+  circles.appendChild(numberList);
+};
+
+const showNumbers = () => {
+  selectSortType();
+  const numbers = document.getElementById("input-numbers").value.split(",");      
+  if (checkNumbers(numbers) !== 0) return;
+  createCircles(numbers);
+}
+
+const swapElement = (e1, e2) =>{
+  const temp = document.createElement('li');
+  temp.className = "number";
+  
 
 }
 
-const bubbleSort = () => {};
-const insertionSort = () => {};
-const mergeSort = () => {};
+const selectSort = () =>{
+  console.log(document.getElementById("numbers-list").querySelectorAll("li"));
+  const numberListNode = document.getElementById("numbers-list");
+  const numberList = [];
+  for(let node of Object.entries(numberListNode)){
+    console.log(node.innerText)
+    numberList.push(parseInt(node.innerText))
+  }
+  console.log(numberList)
+  const length = numbers.length;
+  for(let i=0; i<length-1; i++){
+    for(j=0; j < length-1-i; j++){
+      if(numbers[j]>numbers[j+1]){
+        
+      }
+    }
+  }
+}
+
+const bubbleSort = () => {
+};
+const insertionSort = () => {
+  
+};
+const mergeSort = () => {
+};
+
+const startSort = (type) => {
+  selectSortType();
+  showNumbers();
+  if (type == 1) selectSort();
+  if (type == 2) bubbleSort();
+  if (type == 3) insertionSort();
+  if (type == 4) mergeSort();
+}
